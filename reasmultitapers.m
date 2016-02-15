2c3199c630a2f142e542cf369c94cca13cbd861d
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2016-02-15 15:03:03 +0100

    put inputs check into a separate function

--- reasmultitapers.m
+++ reasmultitapers.m
@@ -1,6 +1,11 @@
-function varargout = reasspecgram(varargin)
+function varargout = reasmultitapers(varargin)
 
-% check parameters
+% check inputs and outputs
+narginchk(1,15);
+nargoutchk(1,6);
+% distribute inputs
+[sig,win,Nw,ovlap,nfft,fs,opts]=parse_inpts(varargin{:});
+opts = check_opts(opts);
 
 % get spectrograms
 
